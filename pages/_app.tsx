ee82cdde1111d27716e9fd123b3439cf634b0e3b
Jarrod Watts <jarrodwatts16@gmail.com>
2022-06-21 17:59:01 +1000

    Add GithubLink

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai;
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Component {...pageProps} />
+      <ThirdwebGuideFooter />
     </ThirdwebProvider>
   );
 }
